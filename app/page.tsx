d73bc02ce650a8e10c3f62360fc7b3b3d9856f20
satrangichutiya <gpyrogramxspam@gmail.com>
2025-06-16 22:44:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { DarkMarketplace } from '@/components/dark-marketplace';
+import { DarkMarketplace } from "@/components/dark-marketplace";
 
 export default function Home() {
   console.log("Direct marketplace access - no authentication required");
 
   return (
-    <div className="min-h-screen">
+    <div
+      className="min-h-screen bg-cover bg-center bg-fixed text-white"
+      style={{
+        backgroundImage:
+          "url('https://media.tenor.com/SFJ6LZs_SVwAAAAC/anime-dark.gif')",
+      }}
+    >
       <DarkMarketplace />
     </div>
   );
